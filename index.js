7d4f24ed5838c53e0d4b3ce0e130bc2512fd29cd
minamhere <github@chrisnolan.org>
2015-02-23 21:42:46 -0500

    Print personal_data table

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM personal_data', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       {   response.render('home', {sayHelloTo: result.rows}); }
+       {   response.render('home', {database: result.rows}); }
     });
   });
 
