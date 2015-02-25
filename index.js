874141befbdf9ff3d56a9a266cd5325343477cc0
spike5634 <jbryannolan@gmail.com>
2015-02-25 16:02:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ app.get('/TaxBrackets', function(request, response) {
 	var taxBrackets = {};
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
    
-    client.query('SELECT * FROM tax_brackets WHERE filing_status_id =1', function(err, result) {
+    client.query('SELECT * FROM tax_brackets 
+    WHERE filing_status_id =1', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
