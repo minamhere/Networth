ac83e1d3ed76085010e711cf9f7a9424d957c707
minamhere <github@chrisnolan.org>
2015-02-25 20:16:41 -0500

    try with a variable again

--- index.js
+++ index.js
@@ -10,20 +10,22 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(request, response) {
-	var users = {};
-	var taxBrackets = {};
+	var users = {}
+	var taxBrackets = {}
+
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-   
+			done();
     client.query('SELECT * FROM personal_data', function(err, result) {
-      done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       {   response.render('home', {database: result.rows}); }
+       {  users = result.rows  }
     });
-	   
+
 	});
-	
+
+	response.render('home', {database: users});
+
 });
 
 
@@ -31,18 +33,18 @@ app.get('/TaxBrackets', function(request, response) {
 	var users = {};
 	var taxBrackets = {};
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-   
+
     client.query('select jurisdiction.name, tax_brackets.minagi, tax_brackets.maxagi from tax_brackets inner join jurisdiction on tax_brackets.jurisdiction_id=jurisdiction.id Where tax_brackets.filing_status_id = 1 AND taxyear = 2015', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       {   
+       {
        	response.render('taxbrackets', {database: result.rows}); }
 	});
-	   
+
 	});
-	
+
 });
 
 app.get('/newuser', function (request, response) {
