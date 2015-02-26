ddd458a8e343688e88e12ef0554c724a3c757604
minamhere <github@chrisnolan.org>
2015-02-25 20:22:08 -0500

    brakcets

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(request, response) {
-	var users = {}
-	var taxBrackets = {}
+	var users = "";
+	var taxBrackets = {};
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 			done();
