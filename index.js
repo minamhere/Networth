e7904cf6f9a6507008b3a3ce91185f5eacbf8620
minamhere <github@chrisnolan.org>
2015-03-05 19:59:04 -0500

    convert to numbers before math

--- index.js
+++ index.js
@@ -104,9 +104,9 @@ app.get('/calcFederal', function(request,response){
 		if (err) { console.error(err); callback(err);}
 		if (data){
 			taxrate = data[0].TaxRate;
-			baseTax = data[0].base_tax;
-			minAGI = data[0].MinAGI;
-			taxDue = baseTax + taxrate*(parseInt(agi)-minAGI)/100;
+			baseTax = parseInt(data[0].base_tax;
+			minAGI = parseInt(data[0].MinAGI;
+			taxDue = baseTax + +taxrate*(parseInt(agi)-minAGI)/100;
 			var responseText = '<p>Federal Tax Due: '+taxDue+'</p>AGI: '+agi;
 
 			response.send(responseText);
