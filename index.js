c984a3899b58289031399f1edc48af1f9cb6febf
minamhere <github@chrisnolan.org>
2015-03-07 12:59:31 -0500

    escape quotes

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ function getFilingStatusFromID(filing_status_id, callback){
 
 function getTaxBracket(jurisdiction, taxyear, agi, callback){
 
-	console.log('SELECT taxrate,base_tax,minagi FROM Tax_Brackets b INNER JOIN jurisdiction j on j.id = b.jurisdiction_id WHERE j.name = '+jurisdiction+' and taxyear = '+taxyear+' and minagi < '+agi+' and maxagi > '+agi);
-	queryDatabase('SELECT taxrate,base_tax,minagi FROM Tax_Brackets b INNER JOIN jurisdiction j on j.id = b.jurisdiction_id WHERE j.name = '+jurisdiction+' and taxyear = '+taxyear+' and minagi < '+agi+' and maxagi > '+agi,function(err,data){
+	console.log('SELECT taxrate,base_tax,minagi FROM Tax_Brackets b INNER JOIN jurisdiction j on j.id = b.jurisdiction_id WHERE j.name = \''+jurisdiction+'\' and taxyear = '+taxyear+' and minagi < '+agi+' and maxagi > '+agi);
+	queryDatabase('SELECT taxrate,base_tax,minagi FROM Tax_Brackets b INNER JOIN jurisdiction j on j.id = b.jurisdiction_id WHERE j.name = \''+jurisdiction+'\' and taxyear = '+taxyear+' and minagi < '+agi+' and maxagi > '+agi,function(err,data){
 		if (err){ console.error(err); callback(err);}
 		callback(null,data);	
 	});
