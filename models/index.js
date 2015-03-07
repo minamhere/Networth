9c47ba0a0a5ed7a866c084b756374b2ba713fbe7
minamhere <github@chrisnolan.org>
2015-03-07 09:05:40 -0500

    use ssl

--- models/index.js
+++ models/index.js
@@ -9,7 +9,10 @@ if (!global.hasOwnProperty('db')) {
       protocol: 'postgres',
       //port:     match[4],
       //host:     match[3],
-      logging:  true //false
+      logging:  true, //false
+      dialectOptions: {
+        ssl: true
+    }
     })
   }
 
