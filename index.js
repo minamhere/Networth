a78edf03f82cb0b1949cb7d2e81b2ce8bd5862f5
minamhere <github@chrisnolan.org>
2015-03-07 14:17:17 -0500

    delete old tax bracket function and jade. delete old home jade.

--- index.js
+++ index.js
@@ -217,24 +217,6 @@ app.get('/admin', function(request, response) {
 	});
 });
 
-app.get('/TaxBrackets', function(request, response) {
-	var users = {};
-	var taxBrackets = {};
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-
-    client.query('select jurisdiction.name, tax_brackets.minagi, tax_brackets.maxagi from tax_brackets inner join jurisdiction on tax_brackets.jurisdiction_id=jurisdiction.id Where tax_brackets.filing_status_id = 1 AND taxyear = 2015', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       {
-       	response.render('taxbrackets', {database: result.rows}); }
-	});
-
-	});
-
-});
-
 app.get('/newuser', function (request, response) {
   response.render('newuser');
 })
