9f683101951cc55807535f87da1734ae4b0c6de6
minamhere <github@chrisnolan.org>
2015-03-15 14:18:57 -0400

    Create folder to store tax info resources

---
+++ Data Sources/tax brackets.sql
@@ -0,0 +1,7 @@
+ INSERT INTO tax_brackets (jurisdiction_id, taxyear, filing_status_id, minagi, maxagi, taxrate, base_tax) VALUES 
+
+ --Colorado
+ (6,2015,1,0,999999,4.63,0),
+ (6,2015,2,0,999999,4.63,0),
+ (6,2015,3,0,999999,4.63,0),
+ (6,2015,4,0,999999,4.63,0);
