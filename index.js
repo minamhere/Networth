5c16f4af0abb73b579cd66f1d526001bd7026917
minamhere <github@chrisnolan.org>
2015-03-15 12:51:28 -0400

    log parsed

--- index.js
+++ index.js
@@ -163,14 +163,17 @@ app.get('/api/calcPaycheck', function(request,response){
 			];
 
 			async.map(brackets,getTaxBracket, function(err, bracketData){
-				console.log(JSON.stringify(bracketData[0]));
 				console.log(JSON.stringify(bracketData[0][0]));
-				console.log(JSON.stringify(bracketData[0].taxrate));
-				console.log(JSON.stringify(bracketData[0][0].taxrate));
-				taxrate = bracketData[0].taxrate/100;
-				marginalIncome = brackets.agi-bracketData[0].minagi;
+				taxrate = bracketData[0][0].taxrate/100;
+				marginalIncome = brackets.agi-bracketData[0][0].minagi;
 				marginalTax = taxrate*marginalIncome;
-				taxDue = +marginalTax + +bracketData[0].base_tax;
+				taxDue = +marginalTax + +bracketData[0][0].base_tax;
+				
+				console.log('taxrate: '+taxrate);
+				console.log('marginalIncome: '+marginalIncome);
+				console.log('marginalTax: '+marginalTax);
+				console.log('taxDue: '+taxDue);
+				
 				callback(null,taxDue);
 			})
 		}]
