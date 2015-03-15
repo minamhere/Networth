28ebceb0636acd4a421b838199518f3f9eee4266
minamhere <github@chrisnolan.org>
2015-03-15 14:42:12 -0400

    upload resources

--- Data Sources/tax brackets.sql
+++ Data Sources/tax brackets.sql
@@ -1,7 +1,79 @@
- INSERT INTO tax_brackets (jurisdiction_id, taxyear, filing_status_id, minagi, maxagi, taxrate, base_tax) VALUES 
+-- 2015 Tax Brackets for all states. 
+-- INSERT INTO tax_brackets (jurisdiction_id, taxyear, filing_status_id, minagi, maxagi, taxrate, base_tax) VALUES 
 
+--Alabama
+INSERT INTO tax_brackets (jurisdiction_id, taxyear, filing_status_id, minagi, maxagi, taxrate, base_tax) VALUES 
+
+ --Alaska
+INSERT INTO tax_brackets (jurisdiction_id, taxyear, filing_status_id, minagi, maxagi, taxrate, base_tax) VALUES 
+(8,2015,1,0,999999,0,0),
+(8,2015,2,0,999999,0,0),
+(8,2015,3,0,999999,0,0),
+(8,2015,4,0,999999,0,0);
+
+--Arizona
+--Arkansas
+--California
+ 
  --Colorado
+ INSERT INTO tax_brackets (jurisdiction_id, taxyear, filing_status_id, minagi, maxagi, taxrate, base_tax) VALUES 
  (6,2015,1,0,999999,4.63,0),
  (6,2015,2,0,999999,4.63,0),
  (6,2015,3,0,999999,4.63,0),
  (6,2015,4,0,999999,4.63,0);
+
+
+--Connecticut
+--Delaware
+--Florida
+--Georgia
+--Hawaii
+--Idaho
+--Illinois
+--Indiana
+--Iowa
+--Kansas
+--Kentucky
+--Louisiana
+--Maine
+--Maryland
+--Massachusetts
+--Michigan
+--Minnesota
+--Mississippi
+--Missouri
+--Montana
+--Nebraska
+--Nevada
+--New Hampshire
+--New Jersey
+--New Mexico
+--New York
+--North Carolina
+--North Dakota
+--Ohio
+--Oklahoma
+--Oregon
+--Pennsylvania
+--Rhode Island
+--South Carolina
+--South Dakota
+--Tennessee
+--Texas
+--Utah
+--Vermont
+--Virginia
+--Washington
+--West Virginia
+--Wisconsin
+--Wyoming
+
+--American Samoa
+--District of Columbia
+--Federated States of Micronesia
+--Guam
+--Marshall Islands
+--Northern Mariana Islands
+--Palau
+--Puerto Rico
+--Virgin Islands
