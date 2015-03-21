fb3d8c816341f3d38eceed6be9c7f82b2c83b3ab
minamhere <github@chrisnolan.org>
2015-03-21 12:58:53 -0400

    first draft of special handling for state. VA only right now.

--- index.js
+++ index.js
@@ -25,6 +25,101 @@ function queryDatabase(queryText,callback){
 	});
 }
 
+function handleState(stateInfo, callback){
+	//stateInfo = {jurisdiction_id:stateID, income:income, retirement:retirement, taxyear: taxyear, filingStatus: filingStatus};
+	switch(stateInfo.jurisdiction_id){
+		case 3: // Virginia
+			async.waterfall([
+				function standardDeductions (callback){
+					getDeductionsExemptions(stateInfo.jurisdiction_id,stateInfo.taxyear,stateInfo.filingStatus, function(err,data){
+						for (var deductionIndex in data){
+							switch(data[deductionIndex].deduction_exemption_type){
+								case 1: // 1 = Standard Deduction
+									stateStandardDeduction = data[deductionIndex].amount;
+									break;
+								case 2:// 2 = Personal Exemption
+									statePersonalExemption = data[deductionIndex].amount;
+									break;
+							};
+						};
+					};
+					callback(null,stateStandardDeduction, statePersonalExemption);
+				},
+				function calcAGI (stateStandardDeduction, statePersonalExemption, callback) {
+					stateAGI = stateInfo.income-stateInfo.retirement-stateStandardDeduction-statePersonalExemption;
+					callback(null, stateAGI);
+				}
+				],
+				function calcStateTax (err, stateAGI) {
+					stateBracket = {jurisdiction_id:stateID, agi:stateAGI, taxyear: taxyear, filingStatus: filingStatus};
+					getTaxDue(stateBracket,callback);
+
+					callback(null,stateTaxDue);
+				});
+			});
+			break;
+		case 6: // Colorado
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+		case 19:
+		case 20:
+		case 21:
+		case 22:
+		case 23:
+		case 24:
+		case 25:
+		case 26:
+		case 27:
+		case 28:
+		case 29:
+		case 30:
+		case 31:
+		case 32:
+		case 33:
+		case 34:
+		case 35:
+		case 36:
+		case 37:
+		case 38:
+		case 39:
+		case 40:
+		case 41:
+		case 42:
+		case 43:
+		case 44:
+		case 45:
+		case 46:
+		case 47:
+		case 48:
+		case 49:
+		case 50:
+		case 51:
+		case 52:
+		case 53:
+		case 54:
+		case 55:
+		case 56:
+		case 57:
+		case 58:
+		case 59:
+		case 60:
+		case 61:
+		case 62:
+		case 63:
+
+	}
+
+}
 function getUserList(callback){
 	queryDatabase('SELECT * FROM personal_data',callback);
 }
@@ -144,15 +239,19 @@ app.get('/api/calcPaycheck', function(request,response){
 				};
 
 				fedAGI = income-retirement-fedStandardDeduction-fedPersonalExemption;
-				//stateAGI = income-retirement-stateStandardDeduction-statePersonalExemption;
-
 				callback(null,{fedAGI:fedAGI,ssAGI:income,medicareAGI:income});
 			});
 		},
-		getStateTax:['getDedExempt', function(callback, results){
-			var stateBracket = {jurisdiction_id:stateID, agi:results.getDedExempt.fedAGI, taxyear: taxyear};
-			getTaxDue(stateBracket,callback);
-		}],
+		getStateTax:function(callback, results){
+			var stateInfo = {
+				jurisdiction_id:stateID, 
+				income:income, 
+				retirement:retirement, 
+				taxyear: taxyear, 
+				filingStatus: filingStatus
+			};
+			handleState(stateInfo, callback);
+		},
 		getFedTax:['getDedExempt', function(callback,results){
 			var brackets = [
 				{jurisdiction_id:1, agi:results.getDedExempt.fedAGI, taxyear: taxyear},
