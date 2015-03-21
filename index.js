91d779e0d28af7c732311936240cc49693bea4a6
minamhere <github@chrisnolan.org>
2015-03-21 13:24:47 -0400

    use stateInfo. for variable names. Also put other state ids in "".

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function handleState(stateInfo, callback){
 				}
 				],
 				function(err, stateAGI) {
-					stateBracket = {jurisdiction_id:stateID, agi:stateAGI, taxyear: taxyear, filingStatus: filingStatus};
+					stateBracket = {jurisdiction_id:stateInfo.stateID, agi:stateAGI, taxyear: stateInfo.taxyear, filingStatus: stateInfo.filingStatus};
 					console.log('state bracket: '+JSON.stringify(stateBracket));
 					getTaxDue(stateBracket,function(data){
 						callback(null,data);
@@ -68,64 +68,64 @@ function handleState(stateInfo, callback){
 				}
 			);
 			break;
-		case 6: // Colorado
-		case 7:
-		case 8:
-		case 9:
-		case 10:
-		case 11:
-		case 12:
-		case 13:
-		case 14:
-		case 15:
-		case 16:
-		case 17:
-		case 18:
-		case 19:
-		case 20:
-		case 21:
-		case 22:
-		case 23:
-		case 24:
-		case 25:
-		case 26:
-		case 27:
-		case 28:
-		case 29:
-		case 30:
-		case 31:
-		case 32:
-		case 33:
-		case 34:
-		case 35:
-		case 36:
-		case 37:
-		case 38:
-		case 39:
-		case 40:
-		case 41:
-		case 42:
-		case 43:
-		case 44:
-		case 45:
-		case 46:
-		case 47:
-		case 48:
-		case 49:
-		case 50:
-		case 51:
-		case 52:
-		case 53:
-		case 54:
-		case 55:
-		case 56:
-		case 57:
-		case 58:
-		case 59:
-		case 60:
-		case 61:
-		case 62:
-		case 63:
+		case "6": // Colorado
+		case "7":
+		case "8":
+		case "9":
+		case "10":
+		case "11":
+		case "12":
+		case "13":
+		case "14":
+		case "15":
+		case "16":
+		case "17":
+		case "18":
+		case "19":
+		case "20":
+		case "21":
+		case "22":
+		case "23":
+		case "24":
+		case "25":
+		case "26":
+		case "27":
+		case "28":
+		case "29":
+		case "30":
+		case "31":
+		case "32":
+		case "33":
+		case "34":
+		case "35":
+		case "36":
+		case "37":
+		case "38":
+		case "39":
+		case "40":
+		case "41":
+		case "42":
+		case "43":
+		case "44":
+		case "45":
+		case "46":
+		case "47":
+		case "48":
+		case "49":
+		case "50":
+		case "51":
+		case "52":
+		case "53":
+		case "54":
+		case "55":
+		case "56":
+		case "57":
+		case "58":
+		case "59":
+		case "60":
+		case "61":
+		case "62":
+		case "63":
 		default: 
 			console.log('Default state case');
 
