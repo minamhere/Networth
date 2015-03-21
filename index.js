90227ca988325ed954852be0963c85072f23652b
minamhere <github@chrisnolan.org>
2015-03-21 13:41:14 -0400

    Add state name comments to each case.

--- index.js
+++ index.js
@@ -70,63 +70,63 @@ function handleState(stateInfo, callback){
 			);
 			break;
 		case "6": // Colorado
-		case "7":
-		case "8":
-		case "9":
-		case "10":
-		case "11":
-		case "12":
-		case "13":
-		case "14":
-		case "15":
-		case "16":
-		case "17":
-		case "18":
-		case "19":
-		case "20":
-		case "21":
-		case "22":
-		case "23":
-		case "24":
-		case "25":
-		case "26":
-		case "27":
-		case "28":
-		case "29":
-		case "30":
-		case "31":
-		case "32":
-		case "33":
-		case "34":
-		case "35":
-		case "36":
-		case "37":
-		case "38":
-		case "39":
-		case "40":
-		case "41":
-		case "42":
-		case "43":
-		case "44":
-		case "45":
-		case "46":
-		case "47":
-		case "48":
-		case "49":
-		case "50":
-		case "51":
-		case "52":
-		case "53":
-		case "54":
-		case "55":
-		case "56":
-		case "57":
-		case "58":
-		case "59":
-		case "60":
-		case "61":
-		case "62":
-		case "63":
+		case "7": // Alabama
+		case "8": // Alaska
+		case "9": // Arizona 
+		case "10": // Arkansas
+		case "11": // California
+		case "12": // Connecticut
+		case "13": // Delaware
+		case "14": // Florida
+		case "15": // Georgia
+		case "16": // Hawaii
+		case "17": // Idaho
+		case "18": // Illinois
+		case "19": // Indiana
+		case "20": // Iowa
+		case "21": // Kansas
+		case "22": // Kentucky
+		case "23": // Louisiana
+		case "24": // Maine
+		case "25": // Maryland
+		case "26": // Massachusetts
+		case "27": // Michigan
+		case "28": // Minnesota
+		case "29": // Mississippi
+		case "30": // Missouri
+		case "31": // Montana
+		case "32": // Nebraska
+		case "33": // Nevada
+		case "34": // New Hampshire
+		case "35": // New Jersey
+		case "36": // New Mexico
+		case "37": // New York
+		case "38": // North Carolina
+		case "39": // North Dakota
+		case "40": // Ohio
+		case "41": // Oklahoma
+		case "42": // Oregon
+		case "43": // Pennsylvania
+		case "44": // Rhode Island
+		case "45": // South Carolina
+		case "46": // South Dakota
+		case "47": // Tennessee
+		case "48": // Texas
+		case "49": // Utah
+		case "50": // Vermont
+		case "51": // Washington
+		case "52": // West Virginia
+		case "53": // Wisconsin
+		case "54": // Wyoming
+		case "55": // American Samoa
+		case "56": // District of Columbia
+		case "57": // Federated States of Micronesia
+		case "58": // Guam
+		case "59": // Marshall Islands
+		case "60": // Northern Mariana Islands
+		case "61": // Palau
+		case "62": // Puerto Rico
+		case "63": // Virgin Islands
 		default: 
 			console.log('Default state case');
 
@@ -270,7 +270,6 @@ app.get('/api/calcPaycheck', function(request,response){
 				{jurisdiction_id:1, agi:results.getDedExempt.fedAGI, taxyear: taxyear},
 				{jurisdiction_id:4, agi:results.getDedExempt.ssAGI, taxyear: taxyear},
 				{jurisdiction_id:5, agi:results.getDedExempt.medicareAGI, taxyear: taxyear},
-				
 			];
 
 			async.map(brackets, getTaxDue, callback);
