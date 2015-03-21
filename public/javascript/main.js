99066bebc30c87063c3fecf5d2ba056bd3921ac8
minamhere <github@chrisnolan.org>
2015-03-21 09:56:53 -0400

    print more variables

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -41,9 +41,18 @@ $(function(){
 			dependents: $('#dependents').val()
 		 };
 		$.get( '/api/calcPaycheck',parameters, function(data) {
-			// {"fedTax":"$518.23","ssTax":"$361.67","medTax":"$84.58","stateTax":"$208.88","stateName":"Virginia","totalTax":"$1,173.36","retirement":"18000","paySchedule":"Monthly","takehomePay":"$3,159.98"}
+			// {"fedTax":"$518.23","ssTax":"$361.67","medTax":"$84.58","stateTax":"$208.88","stateName":"Virginia",
+			//"totalTax":"$1,173.36","retirement":"18000","paySchedule":"Monthly","takehomePay":"$3,159.98"}
 			paycheckData = $.parseJSON(data);
 			$('#fedTax').html(paycheckData.fedTax);
+			$('#ssTax').html(paycheckData.ssTax);
+			$('#medTax').html(paycheckData.medTax);
+			$('#stateTax').html(paycheckData.stateTax);
+			$('#stateName').html(paycheckData.stateName);
+			$('#totalTax').html(paycheckData.totalTax);
+			$('#retirementContributions').html(paycheckData.retirement);
+			$('#paySchedule').html(paycheckData.paySchedule);
+			$('#takehomePay').html(paycheckData.takehomePay);
 		});
 	});
 });
