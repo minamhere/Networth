6cbccf99fccac86c10efa9217481bb51fd1e7e43
minamhere <github@chrisnolan.org>
2015-03-22 15:24:29 -0400

    redirect to paycheck when you go to '/' . add withholding pdf.

--- index.js
+++ index.js
@@ -252,9 +252,13 @@ app.get('/admin', function (request, response) {
 	});
 });
 
+app.get('/',function (request, response) {
+  response.redirect('/paycheck');
+});
+
 app.get('/newuser', function (request, response) {
   response.render('newuser');
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running at localhost:' + app.get('port'));
