5066a7e8498031fe7ff02c4e2499b77993a4c1f3
minamhere <github@chrisnolan.org>
2015-03-29 17:09:11 -0400

    change option loop to use different id/values

--- index.js
+++ index.js
@@ -288,7 +288,7 @@ app.get('/',function (request, response) {
 });
 
 app.get('/angular',function (request, response) {
-        response.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        response.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
  });
 
 app.get('/newuser', function (request, response) {
