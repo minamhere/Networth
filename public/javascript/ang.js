1db1830efa98f5fbdfdf5b8136d2ffa7c1425747
minamhere <github@chrisnolan.org>
2015-04-02 19:27:30 -0400

    Revert 50ddb0e..5f7868c
    This rolls back to commit 50ddb0ebbfb9d0ec8524f1e786c0287ef358b8d4.

--- public/javascript/ang.js
+++ public/javascript/ang.js
@@ -2,10 +2,6 @@ angular.module('paycheckCalculator', ["chart.js"])
 	.controller('PaycheckController', function($scope, $http) {
 		//var paycheck = this;
 
-		$scope.labels = ["Federal Income Tax", "Social Security Tax", "Medicare Tax", $scope.state+" Tax", "Retirement Deductions", "After-Tax Deductions", "Take Home Pay"];
-		$scope.data = [$scope.fedTaxPaystub, $scope.ssTaxPaystub, $scope.medicareTaxPaystub, $scope.stateTaxPaystub, $scope.retirementContributionsPaystub, $scope.afterTaxDeductionPaystub, $scope.takehomePayPaystub];
-
-
 	    $http.get('/api/getFilingStatuses').
 			success(function(data, status, headers, config) {
 				// this callback will be called asynchronously
@@ -115,7 +111,9 @@ angular.module('paycheckCalculator', ["chart.js"])
 					$scope.afterTaxDeductionPaystub = paycheckData.afterTaxDeduction;
 					$scope.paySchedulePaystub = paycheckData.paySchedule;
 
-					
+					$scope.labels = ["Federal Income Tax", "Social Security Tax", "Medicare Tax", paycheckData.stateName+" Tax", "Retirement Deductions", "After-Tax Deductions", "Take Home Pay"];
+					$scope.data = [$scope.fedTaxPaystub, $scope.ssTaxPaystub, $scope.medicareTaxPaystub, $scope.stateTaxPaystub, $scope.retirementContributionsPaystub, $scope.afterTaxDeductionPaystub, $scope.takehomePayPaystub];
+
 
 				}).
 				error(function(data, status, headers, config) {
